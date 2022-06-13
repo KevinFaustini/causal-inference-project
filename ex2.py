d931e4721fcb6c29804e46cdc509332864eb07e2
roemers <sybille.roemer@epfl.ch>
2022-06-13 18:33:34 +0200

    ex2 DI done

--- ex2.py
+++ ex2.py
@@ -37,8 +37,8 @@ def MatrixZFct(D,list,t):
                 VectorTemp.append(MarketData[t][j][k])
             MatrixZ.append(VectorTemp)
     else:
+        MatrixZ = []
         for i in range(t):
-            MatrixZ=[]
             for k in list:
                 VectorTemp=[]
                 for j in range(len(D[0])):
@@ -51,44 +51,46 @@ def MatrixZFct(D,list,t):
 
 KList=[0,1,2,3,4,5,6,7,8,9,10,11]
 Klist2=[2,3,4,5,6,7,8,9,10,11]
+PairList1=list(combinations(KList, 2))
 PairList=list(combinations(KList, 2))
-
-Yt = VectorFct(MarketData,1,2)
-Z = MatrixZFct(MarketData, Klist2,2)
-print(len(Yt))
-print(len(Z))
-#print(np.concatenate((Yt,Z),axis=0))
+#
+# Yt = VectorFct(MarketData,1,2)
+# Z = MatrixZFct(MarketData, Klist2,2)[0]
+# X=VectorFct(MarketData,0,2)
+#
+# print(len(np.concatenate((Yt,Z,X),axis=0)))
 
 temp=0
-# DIList=[]
-# while len(PairList)>0:
-#     temp=0
-#     x = PairList[0][0]
-#     y = PairList[0][1]
-#     xy=[x,y]
-#     PairList.pop(0)
-#     UsedList=[n for n in KList if n not in xy]
-#     for t in range(6):
-#         Yt=VectorFct(MarketData,y,t+1)[0]
-#         Z=MatrixZFct(MarketData,UsedList,t)[0]
-#         X=VectorFct(MarketData,x,t)[0]
-#         Y=VectorFct(MarketData,y,t)[0]
-#
+DIList=[]
+while len(PairList)>0:
+     temp=0
+     x = PairList[0][0]
+     y = PairList[0][1]
+     xy=[x,y]
+     PairList.pop(0)
+     UsedList=[n for n in KList if n not in xy]
+     for t in range(6):
+         Yt=VectorFct(MarketData,y,t+1)
+         Z=MatrixZFct(MarketData,UsedList,t)[0]
+         X=VectorFct(MarketData,x,t)
+         Y=VectorFct(MarketData,y,t)
+         Elmnt1=np.linalg.det(np.cov(np.concatenate((Yt, Z), axis=0)))
+         Elmnt2 = np.linalg.det(np.cov(np.concatenate((X,Y, Z), axis=0)))
+         Elmnt3 = np.linalg.det(np.cov(np.concatenate((Y, Z), axis=0)))
+         Elmnt4 = np.linalg.det(np.cov(np.concatenate((X,Yt, Z), axis=0)))
+         temp+=np.log((Elmnt1*Elmnt2)/(Elmnt3*Elmnt4))
+     DI=0.5*temp
+     DIList.append(DI)
+print(len(DIList))
+print(len(PairList1))
 
-        # dimensions = (190,(t+1)+(t+1)*10)
-        # EmptyMatrix=np.zeros(dimensions)
-        # for r in range(190):
-        #     for c in range(t):
-        #        EmptyMatrix.append(Yt[r][c])
-        # print(EmptyMatrix)
 
 
         # Elmnt1= np.linalg.det(np.cov(Yt, Z, bias=True))
         # Elmnt2=np.linalg.det(np.cov(Y,Z,X, bias=True))
         # Elmnt3=np.linalg.det(np.cov(Y,Z, bias=True))
         # Elmnt4= np.linalg.det(np.cov(Yt,Z,X, bias=True))
-        # temp+=log((Elmnt1*Elmnt2)/(Elmnt3*Elmnt4))
-    #DI=temp*1/2
+
     #print(DI)
     #DIList.append(DI)
 
