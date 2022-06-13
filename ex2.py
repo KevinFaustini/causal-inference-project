f43b6e2eca95f0d5afa822bd52b3e64671d8f12d
roemers <sybille.roemer@epfl.ch>
2022-06-13 23:43:59 +0200

    ex2 graph fini mais mmh jsp

--- ex2.py
+++ ex2.py
@@ -2,21 +2,21 @@ from ex1 import *
 from helpers import mat_to_ndarray
 import scipy.io
 import numpy as np
+import pandas as pd
 from itertools import combinations
 import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib import figure
 
-
+#data importation
 MarketPath='/Users/sybil/PycharmProjects/causal-inference-project/market.mat'
 Data=scipy.io.loadmat(MarketPath)
 MarketData=Data["DI"].tolist()
 
-# CovMatrix=np.cov(VectorYArray,MatrixZArray, bias=True)
 
 
-#Function to create a vector given a dataset and a company number
+#Function creation:
 def VectorFct(D,k,t):
     MatrixOne=[]
     t+=1
@@ -24,14 +24,11 @@ def VectorFct(D,k,t):
         Vector=[]
         for j in range(len(D[0])):
             Vector.append(MarketData[i][j][k])
-        #VectorArray = np.array(Vector)
         MatrixOne.append(Vector)
-        #MatrixOneArray=np.array(MatrixOne)
     return MatrixOne
 
-#print(len(VectorFct(MarketData,0,0)))
 
-#function to create Matrix from a Dataset and a list of company.
+
 def MatrixZFct(D,list,t):
     MatrixFinal=[]
     MatrixZ = []
@@ -50,14 +47,13 @@ def MatrixZFct(D,list,t):
                     VectorTemp.append(MarketData[i][j][k])
                 MatrixZ.append(VectorTemp)
     MatrixFinal.append(MatrixZ)
-    #MatrixFinalArray = np.array(MatrixFinal)
     return MatrixFinal
 
 
+#directed information computation:
 KList=[0,1,2,3,4,5,6,7,8,9,10,11]
 PairList1=list(itertools.permutations(KList, 2))
 PairList=list(itertools.permutations(KList, 2))
-
 
 temp=0
 DIList=[]
@@ -73,34 +69,43 @@ while len(PairList)>0:
          Z=MatrixZFct(MarketData,UsedList,t-1)[0]
          X=VectorFct(MarketData,x,t-1)
          Y=VectorFct(MarketData,y,t-1)
-         Elmnt1=np.linalg.det(np.cov(np.concatenate((Yt, Z), axis=0)))
+         Elmnt1 = np.linalg.det(np.cov(np.concatenate((Yt, Z), axis=0)))
          Elmnt2 = np.linalg.det(np.cov(np.concatenate((X,Y, Z), axis=0)))
          Elmnt3 = np.linalg.det(np.cov(np.concatenate((Y, Z), axis=0)))
          Elmnt4 = np.linalg.det(np.cov(np.concatenate((X,Yt, Z), axis=0)))
          temp+=np.log((Elmnt1*Elmnt2)/(Elmnt3*Elmnt4))
      DI=0.5*temp
      DIList.append(DI)
-print(PairList1)
-print(DIList)
 
-
-
+#discovering which nodes are linked:
 Edgesxy=[]
 for i in range(len(DIList)):
     if DIList[i]>0.5:
         Edgesxy.append(1)
     else:
         Edgesxy.append(0)
-
 LinkedNode=[]
+
+#Creation of the Graph
 DG = nx.DiGraph()
 for i in range(len(Edgesxy)):
     if Edgesxy[i]==1:
         LinkedNode.append(PairList1[i])
-print(LinkedNode)
 DG.add_edges_from(LinkedNode)
-nx.draw_circular(DG, with_labels=True)
+nx.draw_circular(DG, with_labels=True, font_weight='bold')
 plt.savefig("GraphEx2.png")
+
+
+#creation of the 12x12 Matrix containing the DIs
+mat=np.zeros((12,12))
+
+for i in range(len(PairList1)):
+    mat[PairList1[i][0],PairList1[i][1]]=DIList[i]
+
+Matrix_frame = pd.DataFrame(mat)
+print(Matrix_frame.to_latex())
+#Matrix_frame.to_excel("Matrix.xlsx")
+
 
 
 
